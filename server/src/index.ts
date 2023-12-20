c95d09da573fa1ea313250119e0dc407b8c1795a
fderen-dev <fderen.dev@gmail.com>
2023-12-20 20:40:17 +0100

    API init

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,6 @@
-const { chromium } = require('playwright');
+import { chromium } from "playwright";
+import express from 'express';
+import bodyParser from 'body-parser';
 
 async function getPage(url: string): Promise<{ page: any, browser: any, context: any }> {
   const browser = await chromium.launch();
@@ -20,23 +22,55 @@ async function scrap(url: string, selector: string): Promise<string> {
   return text;
 }
 
-async function scrapUrls(pageUrl: string, listSelector: string): Promise<string[]> {
-  const { page, browser } = await getPage(pageUrl);
-  const urls = await page.$$eval(listSelector, (elements: any) => elements.map((el: any) => el.href));
-  await browser.close();
+// async function scrapUrls(pageUrl: string, listSelector: string): Promise<string[]> {
+//   const { page, browser } = await getPage(pageUrl);
+//   const urls = await page.$$eval(listSelector, (elements: any) => elements.map((el: any) => el.href));
+//   await browser.close();
 
-  return urls;
-}
+//   return urls;
+// }
+
+// (async () => {
+//   try {
+//     const urls = await scrapUrls('https://www.masterborn.com/career', 'main #positions a');
+//     urls.forEach(async (url: string) => {
+//       const position = await scrap(url, 'main h1');
+//       const salary = await scrap(url, 'main #offer p:first-of-type + div h3');
+//       console.log(position, salary); 
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// })(); 
+
+// curl -X POST http://localhost:3000/api/scrap \
+// -H "Content-Type: application/json" \
+// -d '{"url": "https://www.masterborn.com/career", "selector": "main #positions a"}'
 
 (async () => {
-  try {
-    const urls = await scrapUrls('https://www.masterborn.com/career', 'main #positions a');
-    urls.forEach(async (url: string) => {
-      const position = await scrap(url, 'main h1');
-      const salary = await scrap(url, 'main #offer p:first-of-type + div h3');
-      console.log(position, salary); 
-    });
-  } catch (error) {
-    console.error(error);
-  }
-})();  
+  const app = express();
+  const port = 3000;
+
+  app.use(bodyParser.json());
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+
+  app.post('/api/scrap', async (req, res) => {
+    const { url, selector } = req.body;
+
+    if (!url || !selector) {
+      res.status(400).send('Missing url or selector in request body');
+      return;
+    }
+
+    try {
+      const text = await scrap(url, selector);
+
+      res.status(200).send(text);
+    } catch (error) {
+      res.status(500).send(error.message);
+      return;
+    }
+  });
+})(); 
